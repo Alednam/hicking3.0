650bd8d337468d7cc26141012fecd7fd40bea6cc
Michael <55077190+Alednam@users.noreply.github.com>
2022-06-06 16:46:50 +0300

    second commit need to work on mguides to replace realtors

--- Hiking2/pages/views.py
+++ Hiking2/pages/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import counties_choices,price_choices,bedrooms_choices
+
+
+
+def index(request):
+    listings=Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context={
+        'listings':listings,
+        'counties_choices':counties_choices,
+        'bedrooms_choices': bedrooms_choices,
+        'price_choices':price_choices,
+
+    }
+    return render(request,'pages/index.html' ,context)
+
+def about(request):
+    realtors = Realtor.objects.order_by('-hire_date')
+    # getmvp
+    mvp_realtors= Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+
+    }
+
+    return render(request,'pages/about.html', context)
